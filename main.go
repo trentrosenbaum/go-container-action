737d6b389c69aed93b8ebb53cd1a29ea88cfa774
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2023-07-17 17:37:21 +0100

    Updates with go-github dependency

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/v53/github"
 	"os"
 )
 
@@ -11,4 +13,23 @@ func main() {
 	output := fmt.Sprintf("Hello %s", myInput)
 
 	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
+
+	client := github.NewClient(nil)
+
+	reviewers, _, err := client.PullRequests.ListReviewers(context.Background(), "trentrosenbaum", "go-container-action", 1, &github.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	teams := reviewers.Teams
+	users := reviewers.Users
+
+	for i, v := range teams {
+		fmt.Printf("Team %d: %s", i, v.Name)
+	}
+
+	for i, v := range users {
+		fmt.Printf("User %d: %s", i, v.Name)
+	}
+
 }
