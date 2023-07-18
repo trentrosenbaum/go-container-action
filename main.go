d4d3b8af322375aae8547a9d7318eba11ccb3f0e
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2023-07-18 08:57:18 +0100

    Switch to use github.com/actions-go/toolkit/core

--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/google/go-github/v53/github"
+	"github.com/actions-go/toolkit/core"
 	"os"
 )
 
 func main() {
 	myInput := os.Getenv("INPUT_MYINPUT")
+	core.Debug(fmt.Sprintf("Input set to %s", myInput))
 
-	output := fmt.Sprintf("Hello %s", myInput)
+	myOutput := fmt.Sprintf("Hello %s", myInput)
 
-	fmt.Println(fmt.Sprintf(`::set-output name=myOutput::%s`, output))
-
-	client := github.NewClient(nil)
-
-	reviewers, _, err := client.PullRequests.ListReviewers(context.Background(), "trentrosenbaum", "go-container-action", 1, &github.ListOptions{})
-	if err != nil {
-		return
-	}
-
-	teams := reviewers.Teams
-	users := reviewers.Users
-
-	for i, v := range teams {
-		fmt.Printf("Team %d: %s", i, v.Name)
-	}
-
-	for i, v := range users {
-		fmt.Printf("User %d: %s", i, v.Name)
-	}
-
+	core.SetOutput("myOutput", myOutput)
+	core.Debug(fmt.Sprintf("Output set to %s", myOutput))
 }
